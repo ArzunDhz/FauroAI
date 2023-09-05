776862c3cbcbc933a20f851754f0dd04d280a460
Arzun Dhz <dahalarjun404@gmail.com>
2023-09-05 10:24:43 +0545

    fixed

--- config/Price.js
+++ config/Price.js
@@ -33,28 +33,4 @@ export const PriceData = [
       "API access for integration",
     ],
   },
-  {
-    id: 4,
-    title: "Gold",
-    price: "$29.99",
-    features: [
-      "AI-powered image enhancement",
-      "Custom prompt development",
-      "Dedicated account manager",
-      "Exclusive templates and styles",
-      "API access for integration",
-    ],
-  },
-  {
-    id: 4,
-    title: "Gold",
-    price: "$29.99",
-    features: [
-      "AI-powered image enhancement",
-      "Custom prompt development",
-      "Dedicated account manager",
-      "Exclusive templates and styles",
-      "API access for integration",
-    ],
-  },
 ];
